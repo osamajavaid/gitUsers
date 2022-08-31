20ccc8f996bc60bbecc18743695ae2e3994b7ba9
Muhammad Osama Javaid <18pwcse1638@uetpeshawar.edu.pk>
2022-08-31 14:19:51 +0500

    gitusers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Banner from "./Components/Banner/Banner";
+import Header from './Components/Header/Header';
+// import Form from './Components/form/Form';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='font-[poppins]'>
+      <Header></Header>
+      <Banner></Banner>
+      {/* <Form></Form> */}
     </div>
   );
 }
